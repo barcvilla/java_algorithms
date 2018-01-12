9ec70bdd4c071a54ebb109d4986a120a4ab685f4
Carlos Villanueva <ceva_19@hotmail.com>
2018-01-12 15:53:25 -0300

    Chapter 1.1 - Bags Queues and Stacks

---
+++ Java_Algorithms4th/src/com/ch1/collections/TestStack.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ch1.collections;
+import edu.princeton.cs.algs4.Stack;
+/**
+ *
+ * @author barcvilla
+ */
+public class TestStack 
+{
+    public static void testingStack()
+    {
+        Stack<String> stack = new Stack<String>();
+        stack.push("Juan Antonio");
+        stack.push("Fernando Alonso");
+        stack.push("Carlos Alberto");
+        while(!stack.isEmpty())
+        {
+            System.out.println(stack.pop() + " ");
+        }
+    }
+    public static void main(String[] args) {
+        testingStack();
+    }
+}
